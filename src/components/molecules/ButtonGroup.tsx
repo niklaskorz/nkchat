14e85794ad24ceb4dd8155cafc8d9b10e29be950
Niklas Korz <korz.niklask@gmail.com>
2018-02-22 11:05:16 +0100

    Re-format

--- src/components/molecules/ButtonGroup.tsx
+++ src/components/molecules/ButtonGroup.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import styled from 'react-emotion';
 
 const Group = styled('div')`
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
 `;
 
 export default class ButtonGroup extends React.Component {
-    render() {
-        return <Group>{this.props.children}</Group>;
-    }
+  render() {
+    return <Group>{this.props.children}</Group>;
+  }
 }
