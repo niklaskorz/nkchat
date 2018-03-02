f3731ecbda62e1ddbfb2179126530929a50f27fe
Niklas Korz <korz.niklask@gmail.com>
2018-03-02 12:59:29 +0100

    Implement mutation `sendMessage`

--- src/schema/messages/resolvers/Mutation.ts
+++ src/schema/messages/resolvers/Mutation.ts
@@ -1,1 +1,26 @@
-export default {};
+import { InstanceType, Message, MessageModel } from 'models';
+import Context from 'Context';
+
+interface SendMessageInput {
+  roomId: string;
+  content: string;
+}
+
+export default {
+  async sendMessage(
+    root: any,
+    { input }: { input: SendMessageInput },
+    ctx: Context,
+  ): Promise<InstanceType<Message>> {
+    const viewer = ctx.state.viewer;
+    if (!viewer) {
+      throw new Error('Authentication required');
+    }
+
+    return await MessageModel.create({
+      content: input.content,
+      author: viewer.id,
+      room: input.roomId,
+    });
+  },
+};
