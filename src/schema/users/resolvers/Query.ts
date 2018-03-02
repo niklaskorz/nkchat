793cc04c20c8bae9329307ae70d267b7eb1d9916
Niklas Korz <korz.niklask@gmail.com>
2018-03-02 12:30:36 +0100

    Use `Context.state.viewer` in `Query.viewer`

--- src/schema/users/resolvers/Query.ts
+++ src/schema/users/resolvers/Query.ts
@@ -2,19 +2,8 @@ import { InstanceType, Session, User } from 'models';
 import Context from 'Context';
 
 export default {
-  async viewer(
-    root: any,
-    data: any,
-    ctx: Context,
-  ): Promise<InstanceType<User> | null> {
-    let session = ctx.state.session;
-    if (!session) {
-      return null;
-    }
-    if (!session.populated('user')) {
-      session = await session.populate('user').execPopulate();
-    }
-    return session.user as InstanceType<User>;
+  viewer(root: any, data: any, ctx: Context): InstanceType<User> | null {
+    return ctx.state.viewer || null;
   },
   session(root: any, data: any, ctx: Context): InstanceType<Session> | null {
     return ctx.state.session || null;
