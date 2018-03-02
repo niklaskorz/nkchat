077c7e18989cece3897bbf0cb56c3bbff764f864
Niklas Korz <korz.niklask@gmail.com>
2018-03-02 13:01:30 +0100

    Update mutation `createRoom`

--- src/schema/rooms/resolvers/Mutation.ts
+++ src/schema/rooms/resolvers/Mutation.ts
@@ -5,27 +5,21 @@ interface CreateRoomInput {
   name: string;
 }
 
-interface CreateRoomPayload {
-  room: InstanceType<Room>;
-}
-
 export default {
   async createRoom(
     root: any,
     { input }: { input: CreateRoomInput },
     ctx: Context,
-  ): Promise<CreateRoomPayload> {
+  ): Promise<InstanceType<Room>> {
     const viewer = ctx.state.viewer;
     if (!viewer) {
       throw new Error('Authentication required');
     }
 
-    const room = await RoomModel.create({
+    return await RoomModel.create({
       name: input.name,
       owner: viewer.id,
       members: viewer.id,
     });
-
-    return { room };
   },
 };
