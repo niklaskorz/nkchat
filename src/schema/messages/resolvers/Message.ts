d8a3f7861e49c74075b4a69a58f31412b143759a
Niklas Korz <korz.niklask@gmail.com>
2018-03-02 12:31:00 +0100

    Implement Message resolvers

--- src/schema/messages/resolvers/Message.ts
+++ src/schema/messages/resolvers/Message.ts
@@ -1,1 +1,37 @@
-export default {};
+import { ObjectID } from 'mongodb';
+import { Message, InstanceType, User, Room } from 'models';
+import Context from 'Context';
+
+export default {
+  createdAt(message: InstanceType<Message>): Date {
+    return (message._id as ObjectID).getTimestamp();
+  },
+  async author(message: InstanceType<Message>): Promise<User> {
+    if (!message.populated('author')) {
+      message = await message.populate('author').execPopulate();
+    }
+    return message.author as User;
+  },
+  viewerIsAuthor(
+    message: InstanceType<Message>,
+    data: any,
+    ctx: Context,
+  ): boolean {
+    const viewer = ctx.state.viewer;
+    if (!viewer) {
+      return false;
+    }
+
+    const authorId = message.populated('author')
+      ? (message.author as InstanceType<User>).id
+      : (message.author as string);
+
+    return viewer.id === authorId;
+  },
+  async room(message: InstanceType<Message>): Promise<Room> {
+    if (!message.populated('room')) {
+      message = await message.populate('room').execPopulate();
+    }
+    return message.room as Room;
+  },
+};
