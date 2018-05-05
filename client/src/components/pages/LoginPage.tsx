3525b45de27941b613a4d4c9b0d4db4ead8214b8
Niklas Korz <korz.niklask@gmail.com>
2018-05-05 13:05:23 +0200

    Update import

--- client/src/components/pages/LoginPage.tsx
+++ client/src/components/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import gql from 'graphql-tag';
 import { ChildProps, compose, graphql, MutationFunc } from 'react-apollo';
 import { Link } from 'react-router-dom';
