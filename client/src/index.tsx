81624110a6a240023ed8c688b50178ffda992551
Niklas Korz <korz.niklask@gmail.com>
2018-05-05 22:01:03 +0200

    Update

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 import 'normalize.css';
 import './global.css';
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
-registerServiceWorker();
